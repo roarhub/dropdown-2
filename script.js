9f1226946157a59547b884909832710608f5a407
czDamian <olebueziedamian@gmail.com>
2025-10-03 20:43:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let otherLinks = document.getElementById("other-links");
+let hiddenLinks = document.getElementById("hidden-links");
+
+otherLinks.addEventListener("click", toggleDropdown);
+
+function toggleDropdown(){
+  hiddenLinks.classList.toggle("hide");``
+}
